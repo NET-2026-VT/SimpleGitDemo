63ce63b2cc49993ea354732be0bbfbcef845e367
DZM77 <32109758+DZM77@users.noreply.github.com>
2026-07-02 10:08:47 +0200

    Refactor greeting messages in Program.cs
    Removed duplicate 'Hello, World!' messages and added a personalized greeting.

--- SimpleGitDemo/Program.cs
+++ SimpleGitDemo/Program.cs
@@ -2,9 +2,9 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Hello, Kalle!");
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+      
         Console.WriteLine("Hello everyone!");
         Console.WriteLine("From dimitris");
     Console.WriteLine("From Git (Haithem)");
