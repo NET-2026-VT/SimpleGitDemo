b976337a905b030bd906a27b38625be7e742b909
DZM77 <32109758+DZM77@users.noreply.github.com>
2026-07-02 10:52:28 +0200

    Update Program.cs

--- SimpleGitDemo/Program.cs
+++ SimpleGitDemo/Program.cs
@@ -7,5 +7,6 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello everyone!");
         Console.WriteLine("From dimitris");
+         Console.WriteLine("From dimitris2");
     }
 }
