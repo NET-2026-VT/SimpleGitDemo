ec52fedd4389cbaab795840425a5cb24a0d2a227
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-07-02 09:45:35 +0200

    Add CW in Dimitris

--- SimpleGitDemo/Program.cs
+++ SimpleGitDemo/Program.cs
@@ -6,5 +6,6 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello everyone!");
+        Console.WriteLine("From dimitris");
     }
 }
